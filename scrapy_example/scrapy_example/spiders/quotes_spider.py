7f09dd63507788db4a1a14fa200762f5c9f8864d
dat nguyen <nguyenhuudat0165@gmail.com>
2023-08-26 22:25:51 +0700

    scraping with login form

--- scrapy_example/scrapy_example/spiders/quotes_spider.py
+++ scrapy_example/scrapy_example/spiders/quotes_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 
+from scrapy.http import FormRequest
 from scrapy_example.items import QuoteItem
 
 
@@ -9,7 +10,8 @@ class QuoteSpider(scrapy.Spider):
     page_number = 2
     start_urls = [
         # "http://quotes.toscrape.com"
-        "http://quotes.toscrape.com/page/2/"
+        # "http://quotes.toscrape.com/page/2/"
+        "http://quotes.toscrape.com/login"
     ]
 
     """
@@ -22,7 +24,43 @@ class QuoteSpider(scrapy.Spider):
 
     """
 
-    def parse(self, response, **kwargs):
+    # def parse(self, response, **kwargs):
+    #     quote_items = QuoteItem()
+
+    #     all_div_quotes = response.css("div.quote")
+    #     for div_quote in all_div_quotes:
+    #         title = div_quote.css("span.text::text").extract()
+    #         author = div_quote.css("small.author::text").extract()
+    #         tags = div_quote.css("a.tag::text").extract()
+
+    #         quote_items["title"] = title
+    #         quote_items["author"] = author
+    #         quote_items["tags"] = tags
+
+    #         yield quote_items
+
+        # next_page = response.css("li.next a::attr(href)").get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self._parse)
+
+        # next_page = f"http://quotes.toscrape.com/page/{str(QuoteSpider.page_number)}/"
+        # if QuoteSpider.page_number < 11:
+        #     QuoteSpider.page_number += 1
+        #     yield response.follow(next_page, callback=self._parse)
+
+    def _parse(self, response, **kwargs):
+        token = response.css("form input::attr(value)").extract_first()
+        return FormRequest.from_response(
+            response,
+            formdata={
+                "csrf_token": token,
+                "username": "aasfdasf",
+                "password": "sdfsgsfg"
+            },
+            callback=self.start_scraping,
+        )
+    
+    def start_scraping(self, response):
         quote_items = QuoteItem()
 
         all_div_quotes = response.css("div.quote")
@@ -36,12 +74,3 @@ class QuoteSpider(scrapy.Spider):
             quote_items["tags"] = tags
 
             yield quote_items
-
-        # next_page = response.css("li.next a::attr(href)").get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, callback=self._parse)
-
-        next_page = f"http://quotes.toscrape.com/page/{str(QuoteSpider.page_number)}/"
-        if QuoteSpider.page_number < 11:
-            QuoteSpider.page_number += 1
-            yield response.follow(next_page, callback=self._parse)
