a35e28e99384fa6f199fcc8e5b259685f682cb30
dat nguyen <nguyenhuudat0165@gmail.com>
2023-08-26 22:07:34 +0700

    scrap with pagination

--- scrapy_example/scrapy_example/spiders/quotes_spider.py
+++ scrapy_example/scrapy_example/spiders/quotes_spider.py
@@ -6,8 +6,10 @@ from scrapy_example.items import QuoteItem
 class QuoteSpider(scrapy.Spider):
     
     name = "quotes"
+    page_number = 2
     start_urls = [
-        "http://quotes.toscrape.com"
+        # "http://quotes.toscrape.com"
+        "http://quotes.toscrape.com/page/2/"
     ]
 
     """
@@ -35,6 +37,11 @@ class QuoteSpider(scrapy.Spider):
 
             yield quote_items
 
-        next_page = response.css("li.next a::attr(href)").get()
-        if next_page is not None:
+        # next_page = response.css("li.next a::attr(href)").get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self._parse)
+
+        next_page = f"http://quotes.toscrape.com/page/{str(QuoteSpider.page_number)}/"
+        if QuoteSpider.page_number < 11:
+            QuoteSpider.page_number += 1
             yield response.follow(next_page, callback=self._parse)
